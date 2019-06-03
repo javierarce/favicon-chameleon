8b59d58dcc098a28120d6b0b3d78a713ab7105a9
Javier Arce <javierarce@users.noreply.github.com>
2019-06-03 09:18:16 +0200

    Removes jQuery dependency

--- README.md
+++ README.md
@@ -3,26 +3,4 @@ Favicon chameleon
 
 Makes your tab disappear when nobody is paying attention.
 
-### Demo 
-
-[Try before you buy](http://javierarce.github.io/jquery.favicon.chameleon).
-
-
-![favicon chameleon](tab.gif)
-
-### Installation
-
-1. Add the ```jquery.favicon.chameleon.js``` file to your HTML page.
-2. Add the ```favicons``` folder.
-2. Launch the plugin: 
-```js
-$(function() {
-    $(this).faviconChameleon({
-      services: ["twitter", "gmail", "tumblr"]
-    });
-});
-```
-
-----
-
-The jQuery plugin was created using the [jQuery Plugin Template](http://kolodny.github.io/blog/blog/2013/12/27/my-favorite-jquery-plugin-template/) by Moshe Kolodny.
+![favicon chameleon](https://favicon-chameleon.glitch.me/favicon-chameleon.gif)
---
+++ script.js
@@ -0,0 +1,117 @@
+const SERVICES = { 
+  tumblr: () => {
+    let title = 'Tumblr'
+    let favicon = 'favicons/tumblr.ico'
+
+    let count = Math.round(Math.random() * 10)
+
+    if (count > 5) {
+      title = `(${count}) Tumblr`
+    }
+
+    return {
+      title, favicon
+    }
+  },
+
+  twitter: () => {
+    let title = 'Twitter'
+    let favicon = 'favicons/twitter.ico'
+
+    return {
+      title, favicon
+    }
+  },
+
+  gmail: () => {
+    let title      = 'Inbox'
+    let count      = Math.round(Math.random() * 12)
+    let emailCount = count
+    let gmailIcon  = 'favicons/gmail_'
+
+    if (count === 11) {
+      count      = 50
+      emailCount = `${count}+`
+    } else if (count === 12) {
+      count      = 100
+      emailCount = `${count}+`
+    }
+
+    let favicon = `${gmailIcon}${count}.ico`
+
+    if (emailCount) {
+      title = `Inbox (${emailCount})`
+    }
+
+    return {
+      title, favicon
+    }
+  } 
+}
+
+class Chameleon {
+  constructor (enabledServices) {
+    this.enabledServices = enabledServices
+    this.services = SERVICES
+    this.hidden = 'hidden'
+    this.visibilityChange = 'visibilitychange'
+
+    this.favicon = document.querySelector("[rel='shortcut icon']").href
+    this.title = document.title
+    
+    this.init()
+  }
+
+  init () {
+    if (typeof document.mozHidden !== 'undefined') {
+      this.hidden = 'mozHidden'
+      this.visibilityChange = 'mozvisibilitychange'
+    } else if (typeof document.msHidden !== 'undefined') {
+      this.hidden = 'msHidden'
+      this.visibilityChange = 'msvisibilitychange'
+    } else if (typeof document.webkitHidden !== 'undefined') {
+      this.hidden = 'webkitHidden'
+      this.visibilityChange = 'webkitvisibilitychange'
+    }
+
+    document.addEventListener(this.visibilityChange, this.handleVisibilityChange.bind(this), false)
+  }
+
+  setDefaultFavicon () {
+    let title = this.title
+    let favicon = this.favicon
+
+    this.updateFavicon({ title, favicon})
+  }
+
+  updateFavicon(data) {
+
+    let randomString = "?v=" + Math.round(Math.random() * 10000000)
+    let link  = document.createElement('link')
+
+    link.type = 'image/x-icon'
+    link.rel  = 'shortcut icon'
+    link.href = data.favicon + randomString
+
+    document.getElementsByTagName('head')[0].querySelector("[rel='shortcut icon']").remove()
+    document.getElementsByTagName('head')[0].appendChild(link)
+    document.title = data.title
+  }
+
+  enableService() {
+    let i = Math.round(Math.random() * (this.enabledServices.length - 1))
+    let service = this.enabledServices[i]
+
+    if (service && this.services[service]) {
+      this.updateFavicon(this.services[service]())
+    }
+  }
+
+  handleVisibilityChange() {
+    if (document[this.hidden]) {
+      this.enableService()
+    } else {
+      this.setDefaultFavicon()
+    }
+  }
+}
